5c2175151185b0ec4e01d1c62961a5fb2bf429ac
leocavalcante <lc@leocavalcante.com>
2018-05-23 19:50:28 -0300

    Hello World

---
+++ lib/app_component.dart
@@ -0,0 +1,8 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app',
+  templateUrl: 'app_component.html',
+  styleUrls: const ['app_component.scss.css'],
+)
+class AppComponent {}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'package:angular/angular.dart';
+
+// ignore: uri_has_not_been_generated
+import 'package:angulardart_boot/app_component.template.dart' as act;
+
+void main() => runApp(act.AppComponentNgFactory);
