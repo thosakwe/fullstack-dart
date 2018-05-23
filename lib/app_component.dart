43c840027bae7eec40e7b092f4ec0f94a2b79ddd
leocavalcante <lc@leocavalcante.com>
2018-05-23 20:04:43 -0300

    Hello Material

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,8 +1,10 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
 
 @Component(
   selector: 'app',
   templateUrl: 'app_component.html',
   styleUrls: const ['app_component.scss.css'],
+  directives: const [MaterialButtonComponent],
 )
 class AppComponent {}
